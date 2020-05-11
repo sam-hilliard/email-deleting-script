9ac4e306b926c94f34c523585f27229a2d588ac3
Sam Hilliard <samhilliard51@gmail.com>
2020-05-11 15:34:08 -0700

    Too many arguments exception
    Fixed the "too many arguments" exception within the login() method.

--- purge_emails.py
+++ purge_emails.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from time import sleep
 
 class EmailPurger:
+
     def __init__(self, e, p):
         self.driver = webdriver.Firefox()
         self.email = e
@@ -14,13 +15,13 @@ class EmailPurger:
         #filling out the email field
         email_xpath = '//*[@id="identifierId"]'
         next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span'
-        self.fill_field(self, self.email, email_xpath, next_btn_path)
+        self.fill_field(self.email, email_xpath, next_btn_path)
         sleep(5)
 
         #filling out the password field
         pass_xpath = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'
         next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span'
-        self.fill_field(self, self.password, pass_xpath, next_btn_path)
+        self.fill_field(self.password, pass_xpath, next_btn_path)
         sleep(5)
 
     def fill_field(self, input, field_path, btn_path):
