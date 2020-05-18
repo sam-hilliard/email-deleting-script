ed599e2eb0a9fc406d1b286870f4b6340f7ae924
Sam Hilliard <samhilliard51@gmail.com>
2020-05-18 15:37:28 -0700

    Added delete functionality
    Emails that are selected are now deleted. FIxed some login issues as well.

--- purge_emails.py
+++ purge_emails.py
@@ -10,21 +10,25 @@ class EmailPurger:
         self.password = p
 
     def login(self):
-        self.driver.get('https://mail.google.com/')
-        sleep(3)
+        #navigating to login page
+        self.driver.get('https://www.google.com/gmail')
+        sleep(5)
+        #sign_in = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[4]/ul[1]/li[2]/a')
+        #sign_in.click()
+
 
         #filling out the email field
         email_xpath = '//*[@id="identifierId"]'
-        next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span'
+        next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span'
         self.fill_field(self.email, email_xpath, next_btn_path)
-        sleep(3)
+        sleep(5)
 
         #filling out the password field
         pass_xpath = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'
-        next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span'
+        next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span/span'
         self.fill_field(self.password, pass_xpath, next_btn_path)
 
-    #deletes mail based on conditions met in shouldDelete()
+    #deletes mail until there is no more mail 
     def delete_mail(self):
         #let the page load
         sleep(10)
@@ -41,11 +45,16 @@ class EmailPurger:
                 checkbox_id = email.find('div', attrs={'role':'checkbox'}).get('id')
                 checkbox = self.driver.find_element_by_id(checkbox_id)
                 checkbox.click()
-
+        
+        #click delete button
+        delete_button = self.driver.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/div[1]/div/div/div[2]/div[3]/div')
+        delete_button.click()
+        sleep(5)
 
     #fills a text field with a given input
     def fill_field(self, input, field_path, btn_path):
         field = self.driver.find_element_by_xpath(field_path)
         field.send_keys(input)
+        sleep(2)
         btn = self.driver.find_element_by_xpath(btn_path)
         btn.click()
