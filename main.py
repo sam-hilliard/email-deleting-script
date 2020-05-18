4b8a9f92fe68a002443dd9095062c7d5fe4881fa
Sam Hilliard <samhilliard51@gmail.com>
2020-05-17 20:54:37 -0700

    Worked on delete_mail()
    Every email that is not marked as importaned or starred is selected with the checkbox.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import purge_emails
 
+print('initializing...')
 purger = purge_emails.EmailPurger('samhilliard51@gmail.com', 'schoolBoiSquid21')
+
+print('logging in...')
 purger.login()
+
+print('testing delete_mail()...')
+purger.delete_mail()
+
+purger.driver.close()
--- purge_emails.py
+++ purge_emails.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from time import sleep
+import bs4 as bs
 
 class EmailPurger:
 
@@ -10,20 +11,39 @@ class EmailPurger:
 
     def login(self):
         self.driver.get('https://mail.google.com/')
-        sleep(5)
+        sleep(3)
 
         #filling out the email field
         email_xpath = '//*[@id="identifierId"]'
         next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span'
         self.fill_field(self.email, email_xpath, next_btn_path)
-        sleep(5)
+        sleep(3)
 
         #filling out the password field
         pass_xpath = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'
         next_btn_path = '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span'
         self.fill_field(self.password, pass_xpath, next_btn_path)
-        sleep(5)
 
+    #deletes mail based on conditions met in shouldDelete()
+    def delete_mail(self):
+        #let the page load
+        sleep(10)
+        #get the html source for soup to parse through
+        html = self.driver.page_source
+        soup = bs.BeautifulSoup(html, 'html.parser')
+        emails = soup.find_all('tr', attrs={'class':'zA'})
+
+        #loop through each email stored in a table row
+        for email in emails:
+            contents = str(email.contents)
+
+            if "Not starred" in contents and "Not important" in contents:
+                checkbox_id = email.find('div', attrs={'role':'checkbox'}).get('id')
+                checkbox = self.driver.find_element_by_id(checkbox_id)
+                checkbox.click()
+
+
+    #fills a text field with a given input
     def fill_field(self, input, field_path, btn_path):
         field = self.driver.find_element_by_xpath(field_path)
         field.send_keys(input)
